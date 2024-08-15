5f273d5dfbbe173a3562ed00bfda599541bc3f77
haseebhamza <syedhaseebhamza333@gmail.com>
2024-08-15 10:04:18 +0500

    make delete and update sub user APi

--- app/userManagement/controller.js
+++ app/userManagement/controller.js
@@ -1,5 +1,6 @@
 const newUser = require("./model");
 const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 async function addNewUser(req, res) {
   const { username, password, role } = req.body;
   if ((!username, !password, !role)) {
@@ -33,4 +34,58 @@ async function getAllSubUser(req, res) {
     res.status(500).json({ message: "Internal server error" });
   }
 }
-module.exports = { addNewUser, getAllSubUser };
+async function deleteSubUser(req, res) {
+  const { id } = req.params; // jab hum req.query lagay gay yahan pay to postman mai ja kr params wala tab select kr wahan id deni ha then url ho ga  ?id=123 is trhn aye ga
+  // jab hum params lagay gay then hum router main b ja kr user/delete/:id dain gay or url mai user/subuser/12343 is thrn postman maim id aye gi
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid ID" });
+  }
+
+  try {
+    const deleteUser = await newUser.findByIdAndDelete(id);
+    if (!deleteUser) {
+      return res
+        .status(404)
+        .json({ message: `Product with ID ${id} not found` });
+    }
+    res
+      .status(200)
+      .json({ message: `Product with ID ${id} deleted successfully` });
+  } catch (error) {
+    res.status(500).json({ message: `Failed to delete product with ID ${id}` });
+  }
+}
+async function handelEditSubuser(req, res) {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid ID" });
+  }
+  const { username, role } = req.body;
+  if ((!username, !role)) {
+    return res.status(400).json({ message: "all filed are required " });
+  }
+  try {
+    const updateSubUser = await newUser.findByIdAndUpdate(
+      id,
+      {
+        username,
+        role,
+      },
+      { new: true, runValidators: true }
+    );
+    if (!updateSubUser) {
+      return res
+        .status(404)
+        .json({ message: `subuser with ID ${id} not found` });
+    }
+    res.status(200).json(updateSubUser);
+  } catch (error) {
+    res.status(500).json({ message: `Failed to update subuser with ID ${id}` });
+  }
+}
+module.exports = {
+  addNewUser,
+  getAllSubUser,
+  deleteSubUser,
+  handelEditSubuser,
+};
--- app/userManagement/route.js
+++ app/userManagement/route.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { addNewUser, getAllSubUser } = require("./controller");
+const {
+  addNewUser,
+  getAllSubUser,
+  deleteSubUser,
+  handelEditSubuser,
+} = require("./controller");
 const authenticateToken = require("../../utils/authMiddleware");
 const router = express();
 
 router.post("/create", authenticateToken, addNewUser);
 router.get("/subuser", authenticateToken, getAllSubUser);
+router.delete("/delete/:id", authenticateToken, deleteSubUser);
+router.patch("/update/:id", authenticateToken, handelEditSubuser);
 
 module.exports = router;
