1a1a6221ce9c307ee534e4ea88f3bfce1cb56cda
talha baig <baigtalha7@gmail.com>
2024-08-29 17:21:47 +0500

    fixes_backend_issues

--- app/item/route.js
+++ app/item/route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../../middleware/authMiddleware");
-const { handleAddItems, handleGetItems } = require("./controller");
+const { handleAddItems, handleGetItems,deleteItem } = require("./controller");
 
 router.post("/create/item", authenticateToken, handleAddItems);
+router.delete("/delete/item/:id", authenticateToken, deleteItem);
 router.get("/all/items", authenticateToken, handleGetItems);
 
 module.exports = router;
