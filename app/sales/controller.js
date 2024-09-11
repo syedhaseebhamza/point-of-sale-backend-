1733017994082c29070ffd91b40be36d562cca88
haseebhamza <syedhaseebhamza333@gmail.com>
2024-09-11 15:20:30 +0500

    create order replace api save data in database

--- app/sales/controller.js
+++ app/sales/controller.js
@@ -3,30 +3,63 @@ const mongoose = require("mongoose");
 
 async function placeOrder(req, res) {
   const { productId, categoryId } = req.query;
-  if (!mongoose.Types.ObjectId.isValid(categoryId, productId)) {
-    return res.status(400).json({ message: "Invalid ID" });
-  }
-  const {variants,quantity,totalPrice,discount} = req.body;
-  if (
 
-    !variants ||
-    !Array.isArray(variants) ||
-    variants.length === 0 ||
-    !quantity||
-    !totalPrice||
-    !discount
-  ) {
-    return res.status(400).json({
-      message:
-        "variants, quantity, totalPrice, and discount are required, and variants must be a non-empty array",
-    });
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    return res.status(400).json({ message: "Invalid ProductId" });
   }
-  for (const variant of variants) {
-    if (!variant.size) {
+  if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+    return res.status(400).json({ message: "Invalid CategoryId" });
+  }
+
+  const { orderdetails, totalPrice } = req.body;
+
+  if (
+    !orderdetails ||
+    !Array.isArray(orderdetails) ||
+    orderdetails.length === 0
+  ) {
+    return res.status(400).json({ message: "Invalid or empty orderdetails" });
+  }
+
+  if (typeof totalPrice !== "number") {
+    return res.status(400).json({ message: "TotalPrice must be a number" });
+  }
+  if (totalPrice == null) {
+    return res.status(400).json({ message: "TotalPrice is required" });
+  }
+
+  for (let i = 0; i < orderdetails.length; i++) {
+    const detail = orderdetails[i];
+
+    if (!detail.productName) {
+      return res
+        .status(400)
+        .json({ message: `ProductName is required for item ${i + 1}` });
+    }
+
+    if (typeof detail.productQuantity !== "number") {
       return res.status(400).json({
-        message: "Each variant must have a non-empty size",
+        message: `ProductQuantity must be a number for item ${i + 1}`,
       });
     }
+  }
+
+  try {
+    const newOrder = new Sales({
+      productId,
+      categoryId,
+      orderdetails,
+      totalPrice,
+    });
+
+    await newOrder.save();
+    return res
+      .status(201)
+      .json({ message: "Order placed successfully", order: newOrder });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error placing order", error: error.message });
   }
 }
 
